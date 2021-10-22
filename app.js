f52247474ad51b9e74b73cc2f690e390aa343594
safwantaliparamba <safwansafu242090@gmail.com>
2021-10-22 18:08:10 +0530

    single comment view added

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const appdata = require('./user.json')
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
+
+const appdata = [
+    {
+        "id": uuid(),
+        "name" : "Safwan taliparamba",
+        "followers":21903,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+        "id": uuid(),
+        "name" : "jubair",
+        "followers":123378,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+
+    },
+    {
+        "id": uuid(),
+        "name" : "hiyas siddique",
+        "followers":643762,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+
+    },
+    {
+        "id": uuid(),
+        "name" : "aswathi achu",
+        "followers":63738,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+
+    },
+    {
+        "id": uuid(),
+        "name" : "Jeff bezos",
+        "followers":327989,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+
+    },
+    {
+        "id": uuid(),
+        "name" : "Elon musk",
+        "followers":457362,
+        "tweet":"Lorem ipsum dolor sit amet consectetur adipisicing elit"
+
+    }
+]
 
 
 app.set('view engine', 'ejs')
@@ -21,9 +65,16 @@ app.get('/comments/new' , (req, res) => {
     res.render('newform')
 })
 app.post('/comments/new', (req, res) => {
+    req.body.id = uuid()
     appdata.push(req.body)
     res.redirect('/comments')
 })
+app.get('/comments/:id' , (req, res) => {
+    const id = req.params.id
+    const cid = appdata.find(c => c.id == id)
+    res.render('details' , {cid})
+})
+
 
 app.listen(3000 , ()=>{
     console.log('listening on port 3000')
